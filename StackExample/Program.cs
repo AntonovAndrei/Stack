7890d24061eb707448a630a586cd7bd134198590
AntonovAndrei <camuy4onkoa4a@yandex.ru>
2022-01-18 20:47:05 +0300

    work project

---
+++ StackExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StackExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Эмуляция Йоды!");
+            
+            var yoda = new Stack<string>();
+            
+            yoda.Push("меня");
+            yoda.Push("зовут");
+            yoda.Push("Йода");
+            
+            LetsIterate(yoda);
+            
+        }
+
+        static void LetsIterate<T>(Stack<T> stack)
+        {
+            foreach (var element in stack)
+            {
+                Print(element);
+            }
+        }
+
+        static void Print<T>(T element)
+        {
+            Console.WriteLine(element);
+        }
+    }
+}
